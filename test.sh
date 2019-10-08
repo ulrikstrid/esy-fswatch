fdabb160a80cac4b0c1e03a831bf83a57f298545
Ulrik Strid <ulrik.strid@outlook.com>
2019-10-08 11:22:55 +0200

    Add esy npm-release step

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ mkdir -p test_dir
 
 cd ./test_dir
 
-echo '{"dependencies": {"fswatch" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'"}}' > package.json
+echo '{"esy": {"release": {"bin": [], "includePackages": ["fswatch"]}}, "dependencies": {"fswatch" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'", "ocaml": "< 4.09.0"}}' > package.json
 
 echo "ESY INSTALL"
 esy install
