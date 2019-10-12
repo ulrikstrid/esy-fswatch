00d33d3acd39d6a34d50e6f2da6ef6c9e183cf68
Ulrik <ulrik.strid@outlook.com>
2019-10-12 21:35:02 +0200

    Try the new prebuilt

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ mkdir -p test_dir
 
 cd ./test_dir
 
-echo '{"esy": {"release": {"bin": [], "includePackages": ["fswatch"]}}, "dependencies": {"fswatch" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'", "ocaml": "< 4.09.0"}}' > package.json
+echo '{"name": "esy-fswatch-prebuilx","esy": {"buildsInSource": "unsafe","build": "EsyPrebuiltApp.exe -action export -target \"#{self.root / os}\" -package \"#{esy-fswatch-prebuilt.install}\""}, "dependencies": {"esy-fswatch-prebuilt" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'", "esy-prebuilt-util": "0.1.2"},"resolutions": {"@reason-native/fs": "facebookexperimental/reason-native:fs.json#399da0bdefa4311af6d3849f2277bfb52024287e","@reason-native/path": "facebookexperimental/reason-native:fp.json#399da0bdefa4311af6d3849f2277bfb52024287e"}}' > package.json
 
 echo "ESY INSTALL"
 esy install
