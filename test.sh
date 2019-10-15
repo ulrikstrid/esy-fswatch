9af7e5e73816a8e487a1c50284250d3588e83f9f
Ulrik <ulrik.strid@outlook.com>
2019-10-15 10:39:27 +0200

    Bump esy-prebuilt-util

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ mkdir -p test_dir
 
 cd ./test_dir
 
-echo '{"name": "esy-fswatch-prebuilx","esy": {"buildsInSource": "unsafe","build": "EsyPrebuiltApp.exe -action export -target \"#{self.root / os}\" -package \"#{esy-fswatch-prebuilt.install}\""}, "dependencies": {"esy-fswatch-prebuilt" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'", "esy-prebuilt-util": "0.1.4"}}' > package.json
+echo '{"name": "esy-fswatch-prebuilx","esy": {"buildsInSource": "unsafe","build": "EsyPrebuiltApp.exe -action export -target \"#{self.root / os}\" -package \"#{esy-fswatch-prebuilt.install}\""}, "dependencies": {"esy-fswatch-prebuilt" : "ulrikstrid/esy-fswatch:esy.json#'$(git rev-parse --short HEAD)'", "esy-prebuilt-util": "0.1.5"}}' > package.json
 
 echo "ESY INSTALL"
 esy install
